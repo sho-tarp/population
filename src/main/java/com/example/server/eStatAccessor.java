0ef5b4eeed7588774d39da6ca6ab8137c755d41a
Nakama <n-shouta@gna.jp>
2016-05-02 15:52:41 +0900

    main()の削除・コード全体の視認性の改善

--- src/main/java/com/example/server/eStatAccessor.java
+++ src/main/java/com/example/server/eStatAccessor.java
@@ -11,8 +11,6 @@ import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.Map;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -98,7 +96,7 @@ public class eStatAccessor {
 			
 			return statsDataId;
 			
-		} catch (Exception e ) {
+		} catch ( Exception e ) {
 			return statsDataId;
 		}
 	}
@@ -174,27 +172,19 @@ public class eStatAccessor {
 	}
 	
 	//XMLデータから各人口を抽出
-	//public String[] parseXML( String xmlData, String prefecture ) {
 	public String[] parseXML( String xmlData, String code ) {
+		
 		String[] dataArray = new String[3];
+		code += "000";
 		
 		try{
-			DocumentBuilderFactory dbf= DocumentBuilderFactory.newInstance();
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			
 			StringReader sr = new StringReader( xmlData );
 			InputSource is = new InputSource( sr );
 			
 			Document document = db.parse( is );
-			
-			code += "000";
-			
-			/*
-			//指定した都道府県名が無い場合
-			if ( code == null ) {
-				System.out.println( code );
-				System.exit(0);
-			}*/
 			
 			//人口データの取得						
 			if ( document.getElementsByTagName( "DATA_INF" ).getLength() == 1 ) {
@@ -216,11 +206,11 @@ public class eStatAccessor {
 						if ( cat02.equals( "001" ) || cat02.equals("") ) {
 						
 							if ( cat01.equals( "001" ) ) {
-								dataArray[0] = String.valueOf(Float.parseFloat(population) / 10);
+								dataArray[0] = String.valueOf( Float.parseFloat( population ) / 10 );
 							} else if ( cat01.equals( "002" ) ) {
-								dataArray[1] = String.valueOf(Float.parseFloat(population) / 10);
+								dataArray[1] = String.valueOf( Float.parseFloat( population ) / 10 );
 							} else if ( cat01.equals( "003" ) ) {
-								dataArray[2] = String.valueOf(Float.parseFloat(population) / 10);
+								dataArray[2] = String.valueOf( Float.parseFloat( population ) / 10 );
 							}
 						}
 					}
@@ -250,11 +240,11 @@ public class eStatAccessor {
 		Element statData = document.createElement( "statData" );
 		document.appendChild( statData );
 		Element totalPopulation = document.createElement( "population" );
-		totalPopulation.appendChild( document.createTextNode( population[0]) );
+		totalPopulation.appendChild( document.createTextNode( population[0] ) );
 		Element malePopulation = document.createElement( "malePopulation" );
-		malePopulation.appendChild( document.createTextNode( population[1]) );
+		malePopulation.appendChild( document.createTextNode( population[1] ) );
 		Element femalePopulation = document.createElement( "femalePopulation" );
-		femalePopulation.appendChild( document.createTextNode( population[2]) );
+		femalePopulation.appendChild( document.createTextNode( population[2] ) );
 		statData.appendChild( totalPopulation );
 		statData.appendChild( malePopulation );
 		statData.appendChild( femalePopulation );
@@ -263,6 +253,7 @@ public class eStatAccessor {
 	}
 	
 	private String documentToString( Document document ) {
+		
 		//DocumentからStringに変換
 		Transformer transformer = null;
 		StringWriter stringWriter = null;
@@ -278,7 +269,7 @@ public class eStatAccessor {
 		transformer.setOutputProperty( "encoding", "utf-8" );
 
 		try {
-			transformer.transform(new DOMSource( document ), new StreamResult( stringWriter ) );
+			transformer.transform( new DOMSource( document ), new StreamResult( stringWriter ) );
 		} catch ( TransformerException e ) {
 			e.printStackTrace();
 		}
@@ -298,19 +289,19 @@ public class eStatAccessor {
 		HashMap<Integer, String[]> populations = new HashMap<Integer, String[]>();
 		
 		//平成の人口データを取得
-		for (int year = 1989; year < 2017; year++) {
-			String surveyYears = String.valueOf(year);
+		for ( int year = 1989; year < 2017; year++ ) {
+			String surveyYears = String.valueOf( year );
 			String statDataId = getStatsDataId( surveyYears );
 			
 			if ( ! statDataId.equals("") ){
 				String xmlData = getStatXML( statDataId );
-				String[] dataArray = parseXML(xmlData, prefectureID );
-				populations.put(Integer.parseInt(surveyYears), dataArray);
+				String[] dataArray = parseXML( xmlData, prefectureID );
+				populations.put(Integer.parseInt( surveyYears ), dataArray );
 				
 				try{
-					Thread.sleep(50);
-				} catch (InterruptedException e){}
-			}
+					Thread.sleep( 50 );
+				} catch ( InterruptedException e ){}
+			} 
 		}
 		
 		return populations;
@@ -331,11 +322,11 @@ public class eStatAccessor {
 		
 		//XMLデータの作成
 		Element root = document.createElement( "prefecture" );
-		root.setAttribute("id", prefectureID);
+		root.setAttribute( "id", prefectureID );
 		document.appendChild( root );
 		
 		//平成の人口データを含むXMLの作成
-		for (int dataYear = 1989; dataYear<2017; dataYear++) {
+		for ( int dataYear = 1989; dataYear<2017; dataYear++ ) {
 
 			System.out.println(dataYear);
 			String[] population;
@@ -348,15 +339,15 @@ public class eStatAccessor {
 			}
 		
 			Element year = document.createElement( "population" );
-			year.setAttribute("year", dYear);
-			root.appendChild(year);
+			year.setAttribute( "year", dYear );
+			root.appendChild( year );
 			
 			Element totalPopulation = document.createElement( "population" );
-			totalPopulation.appendChild( document.createTextNode( population[0]) );
+			totalPopulation.appendChild( document.createTextNode( population[0] ) );
 			Element malePopulation = document.createElement( "malePopulation" );
-			malePopulation.appendChild( document.createTextNode( population[1]) );
+			malePopulation.appendChild( document.createTextNode( population[1] ) );
 			Element femalePopulation = document.createElement( "femalePopulation" );
-			femalePopulation.appendChild( document.createTextNode( population[2]) );
+			femalePopulation.appendChild( document.createTextNode( population[2] ) );
 			year.appendChild( totalPopulation );
 			year.appendChild( malePopulation );
 			year.appendChild( femalePopulation );
@@ -376,37 +367,6 @@ public class eStatAccessor {
 		String xmlData = documentToString( document );
 
 		return xmlData;
-	}
-	
-	public static void main( String[] args ){
-		
-		String applicationID = "ca1cf60ecad380c0abb3dd9cfd9689de7f25eff2";
-		eStatAccessor estataccessor = new eStatAccessor( applicationID );
-		estataccessor.getStatsDataId("2000");
-		//returnYearsXML(applicationID, "47");
-		/*
-		eStatAccessor estataccessor = new eStatAccessor( applicationID );
-		
-		String surveyYears = "";
-		
-		for (int year = 1989; year < 2017; year++) {
-			surveyYears = String.valueOf(year);
-			System.out.println(surveyYears);
-			String statDataId = estataccessor.getStatsDataId( surveyYears );
-			
-			if ( ! statDataId.equals("") ){
-				String xmlData = estataccessor.getStatXML( statDataId );
-				//System.out.println(xmlData);
-				String[] dataArray = estataccessor.parseXML(xmlData, "47");
-				String populationXML = estataccessor.returnXML( dataArray );
-				//System.out.println(populationXML);
-				
-				try{
-					Thread.sleep(1000);
-				} catch (InterruptedException e){}
-			}
-		}
-		*/
 	}
 	
 }
