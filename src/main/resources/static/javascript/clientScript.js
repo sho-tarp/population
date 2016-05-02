ec7676b8bdcf9be8cb6c5d8ac8dcf79d0497b5ce
Nakama <n-shouta@gna.jp>
2016-05-02 10:07:25 +0900

    clientScript.jsの視認性改善

--- src/main/resources/static/javascript/clientScript.js
+++ src/main/resources/static/javascript/clientScript.js
@@ -1,4 +1,3 @@
-//var url = "/api/population/single?id=";
 var xhr = new XMLHttpRequest();
 
 //人口データキャッシュ用
@@ -12,7 +11,7 @@ function getPopulationProc() {
 	var dataObject = {};
 
 	if ( xhr.readyState == 4 ) {
-		if ( xhr.status == 0) {
+		if ( xhr.status == 0 ) {
 			console.log( "通信失敗" );
 		} else {
 			if ( xhr.status === 200 ) {
@@ -39,8 +38,8 @@ function getAllPopulationProc() {
 			if ( xhr.status === 200 ) {
 				console.log( xhr.responseText );
 
-				var index = Number( document.getElementById("prefectures").value);
-				prefecturePopulationXML[index-1] = xhr.responseText;				
+				var index = Number( document.getElementById( "prefectures" ).value );
+				prefecturePopulationXML[ index-1 ] = xhr.responseText;				
 				outputCSV();
 				
 			} else {
@@ -54,39 +53,38 @@ function getAllPopulationProc() {
 function getPopulation() {
 
 	var url = "/api/population/single?id=";
-	var index = document.getElementById("prefectures").value;
+	var index = document.getElementById( "prefectures" ).value;
 
-	if (typeof prefecturePopulationXML[index-1] === "undefined"){
-		document.getElementById( "download" ).removeAttribute("href");
+	if ( typeof prefecturePopulationXML[ index-1 ] === "undefined" ){
+		document.getElementById( "download" ).removeAttribute( "href" );
 	}
 	
 	//キャッシュ機能（一度読み込んだデータはリクエストしない）
-	if (typeof populations[index-1] === "undefined"){
-		XMLRequest(url, index, getPopulationProc);
+	if ( typeof populations[ index-1 ] === "undefined" ){
+		XMLRequest( url, index, getPopulationProc );
 	} else {
 		showPopulation();
 	}	
 };
 
 function XMLRequest( url, pulldownIndex, functionName ) {
-		xhr.open( "GET", url + pulldownIndex);
+		xhr.open( "GET", url + pulldownIndex );
 		xhr.onreadystatechange = functionName;
 		xhr.send();
 }
 
 function showPopulation(){
-	var index = document.getElementById("prefectures").value
+	var index = document.getElementById( "prefectures" ).value
 	
 	document.getElementById( "pop" ).innerHTML
-				= populations[index-1]["population"] + "万人です";
+				= populations[ index-1 ][ "population" ] + "万人です";
 };
 
-
-function parseXML(xmlData) {
+function parseXML( xmlData ) {
 	var dataObject = {};
 
 	var parser = new DOMParser();
-    var dom = parser.parseFromString(xmlData, 'text/xml');
+    var dom = parser.parseFromString( xmlData, 'text/xml' );
     var element = dom.firstChild.firstChild;
 
     dataObject = searchPopulation( element );
@@ -97,10 +95,10 @@ function parseXML(xmlData) {
 function searchPopulation( element ) {
 	var dataObject = {};
 
-	while(element != null) {
+	while( element != null ) {
 		var nodeName = element.nodeName;
 		var nodeText = element.textContent;
-		dataObject[nodeName] = nodeText;
+		dataObject[ nodeName ] = nodeText;
 		element = element.nextSibling;
     }
 
@@ -119,7 +117,6 @@ function outputCSV(){
     
 };
 
-
 function createCSVString( xmlData ) {
 
 	var dataObject = {};
@@ -127,9 +124,10 @@ function createCSVString( xmlData ) {
 	var dom = parser.parseFromString( xmlData, 'text/xml' );
 	var yearElement = dom.firstChild.firstChild;
 
+	//CSVヘッダー部
 	var CSVString = "#year, total, male, female\r\n";
 
-	while (yearElement != null){
+	while ( yearElement != null ){
 		var year = yearElement.getAttribute( "year" );
 		var element = yearElement.firstChild;
 		
@@ -148,16 +146,17 @@ function createCSVString( xmlData ) {
 }
 
 function createCSV( CSVString, index ) {
-	var blob = new Blob([CSVString], {type:"text/csv"});
-	var prefecture = document.getElementById("prefectures").options[Number(index)-1].text;
+	var blob = new Blob( [ CSVString ], { type:"text/csv" } );
+	var prefecture = document.getElementById( "prefectures" ).options[ Number( index ) -1 ].text;
 	var filename = prefecture + ".csv"
 	
-	if (window.navigator.msSaveBlob) {
+	//ブラウザがInternetExplorerの場合
+	if ( window.navigator.msSaveBlob ) {
 		// msSaveOrOpenBlobの場合はファイルを保存せずに開ける
-        window.navigator.msSaveOrOpenBlob(blob, filename); 
+        window.navigator.msSaveOrOpenBlob( blob, filename ); 
         } else {
-        	document.getElementById("download").href = window.URL.createObjectURL(blob);
-        	document.getElementById("download").download = filename;
+        	document.getElementById(　"download"　).href = window.URL.createObjectURL(　blob　);
+        	document.getElementById(　"download"　).download = filename;
         }
 
 }
@@ -165,11 +164,11 @@ function createCSV( CSVString, index ) {
 function getAllPopulation(){
 	
 	var url = "/api/population/all?id=";
-	var index = document.getElementById("prefectures").value;
+	var index = document.getElementById(　"prefectures"　).value;
 
 	document.getElementById( "downloadCSV" ).disabled = true;
 
-	if (typeof prefecturePopulationXML[index-1] === "undefined"){
+	if (　typeof prefecturePopulationXML[　index-1　] === "undefined"　){
 		XMLRequest( url, index, getAllPopulationProc );
 	} else {
 		outputCSV();
